7914c51e9472b22774bcaec8598a7c7502230190
ringo-Yui <82448822+ringo-Yui@users.noreply.github.com>
2021-09-16 19:32:04 +0900

    Create Hey_siri_iwa.py

---
+++ submit/Hey_siri/Hey_siri_iwa.py
@@ -0,0 +1,12 @@
+def Hey_Siri(a): 
+
+    if a=="こんにちは":
+        print("こんにちは")
+    elif a=="名前は": 
+        print("Siriです")
+    elif a=="ありがとう":
+        print("どういたしまして") 
+    else:
+        print("すみません、よく分かりません。")
+x=input()
+Hey_Siri(x)
