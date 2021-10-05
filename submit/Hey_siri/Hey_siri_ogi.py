b86d63c5a2bbbc1430322a8b25f3dfca864c492c
mayukoogi <89843480+mayukoogi@users.noreply.github.com>
2021-10-05 18:06:03 +0900

    Create Hey_siri_ogi.py

---
+++ submit/Hey_siri/Hey_siri_ogi.py
@@ -0,0 +1,21 @@
+
+def Hey_siri():
+    voice=input()
+
+    if voice=="こんにちは":
+        print("こんにちは")
+
+    
+    elif voice=="おはよう":
+        print("おはようございます")
+
+    elif voice=="こんばんは":
+        print("こんばんは")
+
+    elif voice=="名前は？":
+        print("siriです")
+
+    else:
+        print("すみません。よくわかりません。")
+
+Hey_siri()
